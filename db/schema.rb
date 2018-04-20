ffeafeee7622fa49a9a23d2c82bdc4a9612a9068
sanra9 <sanrra999@gmail.com>
2018-04-20 17:18:33 -0500

    Implementa la funcionalidad de registrar usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418122425) do
+ActiveRecord::Schema.define(version: 20180420215930) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "url"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", limit: 100
+    t.string "password_digest"
+    t.string "name", limit: 100
+    t.string "twitter_handle", limit: 50
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
